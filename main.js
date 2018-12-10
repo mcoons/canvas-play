8cd29b9e4b299a058757b95c21485f8911e9e325
Michael Coons <mcoons67@gmail.com>
2018-12-09 23:28:11 -0700

    added palette and ready to remove color wheel

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let width = 801;
+let width = 800;
 let halfWidth = Math.floor(width / 2);
+let palette = [];
+buildPalette();
 
 ////////////////////////////////////////////////////////
 
@@ -15,68 +17,8 @@ backCtx.translate(halfWidth,halfWidth);
 backCtx.fillStyle = "#000000";
 backCtx.fillRect(-halfWidth,-halfWidth,width,width);
 
-let backgroundInterval = scrollingBackground();
-
-////////////////////////////////////////////////////////
-
-function scrollingBackground(){
-    const image = document.getElementById('colorsource');
-
-    let colorwheel = document.getElementById("colorwheel")
-    colorwheel.width = 100;
-    colorwheel.height = 100;
-    colorwheel.style.width = 100;
-    colorwheel.style.height = 100;
-
-    colorTheta = 0;
-
-    let colorCtx = colorwheel.getContext("2d");
-    colorCtx.translate(50,50);
-    colorCtx.drawImage(image, -50, -50, 100, 100);
-
-    let interval = setInterval(scrollColors, 20, backCtx);
-    return interval;
-
-    function scrollColors(ctx) {
-        colorX = Math.cos(colorTheta)*40;
-        colorY = Math.sin(colorTheta)*40;
-        
-        color2X = Math.cos(colorTheta+2*Math.PI/3)*40;
-        color2Y = Math.sin(colorTheta+2*Math.PI/3)*40;
-        
-        color3X = Math.cos(colorTheta+4*Math.PI/3)*40;
-        color3Y = Math.sin(colorTheta+4*Math.PI/3)*40;
-
-        let bColor = colorCtx.getImageData(colorX+50, colorY+50, 1, 1).data;
-        var hex = "#" + ("000000" + rgbToHex(bColor[0]/2, bColor[1]/2, bColor[2]/4)).slice(-6);
-        
-        let bColor2 = colorCtx.getImageData(color2X+50, color2Y+50, 1, 1).data;
-        var hex2 = "#" + ("000000" + rgbToHex(bColor2[0]/2, bColor2[1]/2, bColor2[2]/4)).slice(-6);
-        
-        let bColor3 = colorCtx.getImageData(color3X+50, color3Y+50, 1, 1).data;
-        var hex3 = "#" + ("000000" + rgbToHex(bColor3[0]/2, bColor3[1]/2, bColor3[2]/4)).slice(-6);
-
-        let gradient =ctx.createRadialGradient(0,0,75, 0,0,width/1.5);
-
-        // Add three color stops
-        gradient.addColorStop(0, hex);
-        gradient.addColorStop(.3, hex2);
-        gradient.addColorStop(.9, hex3);
-
-        // Set the fill style and draw a rectangle
-        ctx.fillStyle = gradient;
-        ctx.fillRect(-halfWidth, -halfWidth, width, width);
-
-        colorTheta += .005;
-        if (colorTheta >= 2*Math.PI) colorTheta = 0;
-    }
-
-    function rgbToHex(r, g, b) {
-        if (r > 255 || g > 255 || b > 255)
-            throw "Invalid color component";
-        return ((r << 16) | (g << 8) | b).toString(16);
-    }
-}
+let colorTheta = 0;
+let index = 0;
 
 ////////////////////////////////////////////////////////
 
@@ -92,27 +34,31 @@ let ctx = canvas.getContext("2d");
 ctx.translate(halfWidth, halfWidth);
 clear(ctx);
 
-function clear(ctx) {
-  ctx.clearRect(-halfWidth, -halfWidth, width, width);
-}
+//////////////////////////////////////////////////////////////////////
 
+let showBackground = true;
+let showTrig = true;
+let showCircles = false;
+let showCircles2 = false;
+let showCircles3 = false;
+let showKoch = false;
 
-// for (let r = halfWidth/3; r <= halfWidth; r+=1.5) {
-//     let color = `rgba(0,0,${255-r+halfWidth/8},.2)`;
-//     drawCircle(ctx, 0, 0, r, color);
-// }
+let backgroundInterval = null;
 
-// drawCircle(ctx, 0, 0, halfWidth, "red");
+if (showBackground) backgroundInterval = scrollingBackground();
+if (showTrig) trig();
+if (showCircles) circles(0, 0, (width-1)/4, 5);
+if (showCircles2) circles2(0, 0, (width-1)/5, 7, null);
+if (showCircles3) circles3(0, 0, (width - 1) / 6, 7, null);
+if (showKoch) koch();
 
 //////////////////////////////////////////////////////////////////////
 
-// trig(ctx);
-
-function trig(ctx) {
-  // ctx.shadowColor = "rgba(20,20,20,.2)";
-  // ctx.shadowOffsetX = 5;
-  // ctx.shadowOffsetY = 5;
-  // ctx.shadowBlur = 1;
+function trig() {
+  ctx.shadowColor = "rgba(20,20,20,.2)";
+  ctx.shadowOffsetX = 5;
+  ctx.shadowOffsetY = 5;
+  ctx.shadowBlur = 1;
 
   ctx.fillStyle = "#FFFFFF";
   drawText(ctx, 5, -6, "(0,0)", "12px serif");
@@ -136,12 +82,6 @@ function trig(ctx) {
 
 //////////////////////////////////////////////////////////////////////
 
-// for (let r = halfWidth/2; r <= halfWidth; r+=.5) {
-//     let color = `rgba(${255-r+halfWidth/3},0,0,.7)`;
-//     drawCircle(ctx, 0, 0, r, color);
-// }
-// circles(0, 0, (width-1)/4, 5);
-
 function circles(x, y, r, i) {
   drawCircle(ctx, x, y, r, "magenta");
 
@@ -154,12 +94,6 @@ function circles(x, y, r, i) {
 }
 
 //////////////////////////////////////////////////////////////////////
-
-// for (let r = halfWidth/5; r <= halfWidth; r+=.5) {
-//     let color = `rgba(0,${255-r+halfWidth/8},0,.7)`;
-//     drawCircle(ctx, 0, 0, r, color);
-// }
-// circles2(0, 0, (width-1)/5, 7, null);
 
 function circles2(x, y, r, i, dir) {
   drawCircle(ctx, x, y, r, "red");
@@ -174,12 +108,6 @@ function circles2(x, y, r, i, dir) {
 
 //////////////////////////////////////////////////////////////////////
 
-// for (let r = halfWidth/3; r <= halfWidth; r+=.5) {
-//     let color = `rgba(0,0,${255-r+halfWidth/8},.7)`;
-//     drawCircle(ctx, 0, 0, r, color);
-// }
-// circles3(0, 0, (width - 1) / 6, 7, null);
-
 function circles3(x, y, r, i, dir) {
   drawCircle(ctx, x, y, r, `#${i + 2}${i + 2}${i + 2}${i + 2}FF`);
 
@@ -191,38 +119,10 @@ function circles3(x, y, r, i, dir) {
   if (dir != "t") circles3(x, y - r - r / 2, r / 2, i - 1, "b");
 }
 
-// function drawCircle(ctx, cx, cy, radius, color){
-//     ctx.fillStyle = color;
-//     for (let deg = 0; deg < 360; deg+=.1) {
-//         let rad = deg*Math.PI/180;
-
-//         let x = cx + Math.cos(rad) * radius;
-//         let y = cy + Math.sin(rad) * radius;
-
-//         drawPoint(ctx, x, y);
-//     }
-// }
-
 //////////////////////////////////////////////////////////////////////
 
-
-// var gradient = ctx.createRadialGradient(0,0,100, 0,0,500);
-
-// Add three color stops
-// gradient.addColorStop(0, 'red');
-// gradient.addColorStop(.3, 'purple');
-// gradient.addColorStop(.4, 'purple');
-// gradient.addColorStop(.7, 'blue');
-// gradient.addColorStop(1, 'green');
-
-// Set the fill style and draw a rectangle
-// ctx.fillStyle = gradient;
-// ctx.fillRect(-halfWidth, -halfWidth, width, width);
-
-koch();
-
 function koch() {
-  ctx.strokeStyle = "#00FFFF";
+  ctx.strokeStyle = "#99FFFF";
 
   for (let r = 2.2; r <= 80; r *= 1.34) {
     let radius = width / r;
@@ -298,6 +198,140 @@ function drawText(ctx, x, y, txt, font) {
   ctx.fillText(txt, x, y);
 }
 
+function clear(ctx) {
+  ctx.clearRect(-halfWidth, -halfWidth, width, width);
+}
+
+// index:   into color palette
+// percent:  -1 = black, 1 = white
+function getColor(index, percent){   
+  let color = palette[index].color;
+  let newColor = shadeRGBColor(color, percent); 
+  return newColor;
+}
+
+// percent:  -1 = black, 1 = white
+function shadeRGBColor(color, percent) {
+  var f=color.split(","),
+      t=percent<0?0:255,
+      p=percent<0?percent*-1:percent,
+      R=parseInt(f[0].slice(4)),
+      G=parseInt(f[1]),
+      B=parseInt(f[2]);
+
+  return "rgb("+(Math.round((t-R)*p)+R)+","+
+                (Math.round((t-G)*p)+G)+","+
+                (Math.round((t-B)*p)+B)+")";
+}
+
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+function buildPalette(){
+  let r=255, g=0, b=0;
+
+  for (g=0; g<=255; g++){ palette.push({r,g,b,color:`rgb(${r},${g},${b})`});}
+  g--;
+
+  for (r=254; r>=0; r--){ palette.push({r,g,b,color:`rgb(${r},${g},${b})`});}
+  r++;
+
+  for (b=1; b<=255; b++){ palette.push({r,g,b,color:`rgb(${r},${g},${b})`});}
+  b--;
+
+  for (g=254; g>=0; g--){ palette.push({r,g,b,color:`rgb(${r},${g},${b})`});}
+  g++;
+
+  for (r=1; r<=255; r++){ palette.push({r,g,b,color:`rgb(${r},${g},${b})`});}
+  r--;
+
+  for (b=254; b>0; b--){ palette.push({r,g,b,color:`rgb(${r},${g},${b})`});}
+  b++;
+}
+
+////////////////////////////////////////////////////////
+
+function scrollingBackground(){
+  // const image = document.getElementById('colorsource');
+
+  // let colorwheel = document.getElementById("colorwheel")
+  // colorwheel.width = 100;
+  // colorwheel.height = 100;
+  // colorwheel.style.width = 100;
+  // colorwheel.style.height = 100;
+
+  // let colorCtx = colorwheel.getContext("2d");
+  // colorCtx.translate(50,50);
+  // colorCtx.drawImage(image, -50, -50, 100, 100);
+
+  // let interval = setInterval(scrollColors, 20, backCtx);
+  let interval = setInterval(scrollColors2, 20, backCtx, -.75);
+  return interval;
+
+  // function scrollColors(ctx) {
+  //     colorX = Math.cos(colorTheta)*40;
+  //     colorY = Math.sin(colorTheta)*40;
+      
+  //     color2X = Math.cos(colorTheta+2*Math.PI/3)*40;
+  //     color2Y = Math.sin(colorTheta+2*Math.PI/3)*40;
+      
+  //     color3X = Math.cos(colorTheta+4*Math.PI/3)*40;
+  //     color3Y = Math.sin(colorTheta+4*Math.PI/3)*40;
+
+  //     let bColor = colorCtx.getImageData(colorX+50, colorY+50, 1, 1).data;
+  //     var hex = "#" + ("000000" + rgbToHex(bColor[0]/4, bColor[1]/4, bColor[2]/2)).slice(-6);
+      
+  //     let bColor2 = colorCtx.getImageData(color2X+50, color2Y+50, 1, 1).data;
+  //     var hex2 = "#" + ("000000" + rgbToHex(bColor2[0]/4, bColor2[1]/4, bColor2[2]/2)).slice(-6);
+      
+  //     let bColor3 = colorCtx.getImageData(color3X+50, color3Y+50, 1, 1).data;
+  //     var hex3 = "#" + ("000000" + rgbToHex(bColor3[0]/4, bColor3[1]/4, bColor3[2]/2)).slice(-6);
+
+  //     let gradient =ctx.createRadialGradient(0,0,75, 0,0,width/1.5);
+
+  //     // Add three color stops
+  //     gradient.addColorStop(0, hex);
+  //     gradient.addColorStop(.3, hex2);
+  //     gradient.addColorStop(.9, hex3);
+
+  //     // Set the fill style and draw a rectangle
+  //     ctx.fillStyle = gradient;
+  //     ctx.fillRect(-halfWidth, -halfWidth, width, width);
+
+  //     colorTheta += .005;
+  //     if (colorTheta >= 2*Math.PI) colorTheta = 0;
+  // }
+
+  function scrollColors2(ctx, percent){
+    bColor1 = getColor( index%1530, percent);
+    // bColor2 = getColor( (index+255)%1530, percent);
+    bColor3 = getColor( (index+510)%1530, percent);
+    // bColor4 = getColor( (index+765)%1530, percent);
+    bColor5 = getColor( (index+1020)%1530, percent);
+    // bColor6 = getColor( (index+1275)%1530, percent);
+
+    let gradient =ctx.createRadialGradient(0,0,width/20, 0,0,width/1.5);
+
+    // Add three color stops
+    gradient.addColorStop(0,  bColor1);
+    // gradient.addColorStop(.2, bColor2);
+    gradient.addColorStop(.5, bColor3);
+    // gradient.addColorStop(.6, bColor4);
+    gradient.addColorStop(1, bColor5);
+    // gradient.addColorStop(1,  bColor6);
+
+    ctx.fillStyle = gradient;
+    ctx.fillRect(-halfWidth, -halfWidth, width, width);
+
+    index++;
+    index = index%1530;
+  }
+}
+
+
 ///////////////////////////
 // Properties
 ///////////////////////////
@@ -328,7 +362,7 @@ function drawText(ctx, x, y, txt, font) {
 // arcTo()
 // beginPath() **
 // bezierCurveTo()
-// clearRect()
+// clearRect() **
 // clip()
 // closePath()
 // createImageData()
